2aafeb02fe1bf00af9950f44896246e26180ad0b
shenlianmeng <lianmengshen@gmail.com>
2019-05-16 22:11:00 +0800

    '第一次提交'

---
+++ shop-web/src/main/java/com/shop/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.shop.controller;
+
+/**
+ * Created by ace on 2019/5/16 21:51
+ *
+ * @author shenlianmeng@ysten.com
+ */
+public class IndexController {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to online-shop!!!");
+    }
+
+}
